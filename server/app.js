7cc1348dcba8cdf19a0643350e028add34ce7b4d
Gianni P <perkinsgianni@gmail.com>
2022-03-31 23:36:26 -0400

    Update .env file and app.js

--- server/app.js
+++ server/app.js
@@ -4,46 +4,41 @@
 const express = require('express');
 const morgan = require('morgan');
 const axios = require('axios');
-// const bodyParser = require('body-parser');
+require('dotenv').config();
 
 // create app server
-const app = express()
-const url = 'http://www.omdbapi.com/?apikey=98285b6d';
+const app = express();
+const url = `http://www.omdbapi.com/?apikey=${process.env.API_KEY}`;
 let cache = {};
-// let movies = {};
 
 // middleware: morgan's dev format
-app.use(morgan('dev'))
-// app.use(bodyParser.json());
+app.use(morgan('dev'));
 
 // routes for GET requests
 app.get('/', (req, res) => {
-  // assign request (ex. i=tt3896198) for url as movieKey
+  // assign request (ex. i=tt3896198) for url to movieKey
   let movieKey = req.url;
   
   // if request is movie stored in cache
   if ( cache[movieKey] ) {
     console.log("Retrieving movie from cache...");
     // send movie data from cache as response
-    res.send(cache[movieKey])
-    // console.log( cache[movieKey] )
+    res.send(cache[movieKey]);
+    // console.log(cache[movieKey])
   } else {
-      console.log("Retrieving movie from OMDB...")
+      console.log("Retrieving movie from OMDB...");
       // initiate axios GET request from OMDB API
-      axios.get(`http://www.omdbapi.com${movieKey}&apikey=98285b6d`)
+      axios.get(`http://www.omdbapi.com${movieKey}&apikey=${process.env.API_KEY}`)
         // then() method returns response
-        .then((res) => {
-          // assign data response as movieData
-          let movieData = res.data;
+        .then((response) => {
+          // assign movie data in response to movieData
+          let movieData = response.data;
           // assign movieData to cache object
           cache[movieKey] = movieData;
           // send movie data
-          res.send(movieData)
+          res.send(movieData);
           // console.log(movieData)
         })
-        // .catch((err) => {
-        //     console.log(err)
-        //   })
     }
 });
 
